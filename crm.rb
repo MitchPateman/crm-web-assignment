64a833dc75b733d9dc6015616db79bf2053b49d8
Mitch Pateman <mitch_pateman@hotmail.com>
2016-06-08 14:11:21 -0400

    Added the main menu to index.erb

--- crm.rb
+++ crm.rb
@@ -2,3 +2,10 @@
 # Do NOT copy the CRM class from the old crm assignment, as it won't work
 # at all for the web-based version!
 # You'll have to implement it from scratch.
+
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+	erb :index
+end
